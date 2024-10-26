3b74e6909c41b27ff2bc740ad2897dd9fbc15f92
24900753 <vignesh.vgm9363@gmail.com>
2024-10-26 08:54:35 +0530

    success

---
+++ web.py
@@ -0,0 +1,33 @@
+from http.server import HTTPServer,BaseHTTPRequestHandler
+
+content='''
+<!doctype html>
+<html>
+    <title>LAPTOP</title>
+    <body>
+        LAPTOP CONFIGURATION
+        <TABLE border="10" celpadding="15">
+            <tr><th>SYSTEM CONFIGURATION</th> <th>DESCRIPTION</th></tr>
+            <tr><td>Processor</td> <td> Intel core-i5</td></tr>
+            <tr><td>Primary Memory</td> <td>16GB Ram</td></tr>
+            <tr><td>Secondary Memory</td> <td>512GB ssd</td></tr>
+            <tr><td>OS</td> <td>Windows 11</td></tr>
+            <tr><td>Graphics Card</td> <td>NVIDIA GeForce MX550</td></tr>
+        </TABLE>
+    </body>
+</html>
+
+'''
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print("Get request received...")
+        self.send_response(200) 
+        self.send_header("content-type", "text/html")       
+        self.end_headers()
+        self.wfile.write(content.encode())
+
+print("This is my webserver") 
+server_address =('',8000)
+httpd = HTTPServer(server_address,MyServer)
+httpd.serve_forever()
